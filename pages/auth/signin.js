413ce9a808eac1e8e0b087b1f9cc479ad208f972
REIMI SASAKI <rey.my013@gmail.com>
2022-03-24 20:43:10 -0700

    add nextauth secret

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -20,7 +20,9 @@ function signIn({ providers }) {
               <button
                 className="p-3 bg-blue-500 rounded-lg text-white"
                 onClick={() =>
-                  SignIntoProvider(provider.id, { callbackUrl: '/' })
+                  SignIntoProvider(provider.id, {
+                    callbackUrl: 'https://insta-clone-sooty.vercel.app/',
+                  })
                 }
               >
                 Sign in with {provider.name}
